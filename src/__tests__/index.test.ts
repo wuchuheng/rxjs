30b1dc68503abbdea5005ab289e1dc57eeaa55c5
wuchuheng <root@wuchuheng.com>
2022-01-19 23:44:49 +0800

    tag: 1.0.1

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,13 +1,13 @@
-import SubscriptionService from "../index";
+import SubscriptionService from '../index';
 
 class SubscriptionScheduler {
-    public static fooObserve = new SubscriptionService<string>("")
+  public static fooObserve = new SubscriptionService<string>('');
 }
 
 test('Test Subscription', () => {
-    const patchData: string = "hello";
-    SubscriptionScheduler.fooObserve.subscription((value) => {
-        expect(value).toBe(patchData)
-    })
-    SubscriptionScheduler.fooObserve.next(patchData)
+  const patchData: string = 'hello';
+  SubscriptionScheduler.fooObserve.subscription((value) => {
+    expect(value).toBe(patchData);
+  });
+  SubscriptionScheduler.fooObserve.next(patchData);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,67 @@
-import {useEffect, useState} from "react"
+import { useEffect, useState } from 'react';
 
-type SubscriptionHandler = number
+type SubscriptionHandler = number;
 
 interface SubscriptionServiceInterface<T> {
-   value: T;
+  value: T;
 
-   next(patchData: T): boolean;
+  next(patchData: T): boolean;
 
-   subscription(patchDataCallback:(patchData: T) => void ): SubscriptionHandler;
+  subscription(patchDataCallback: (patchData: T) => void): SubscriptionHandler;
 
-   unSubscription(subscriptionHandler: SubscriptionHandler): boolean;
+  unSubscription(subscriptionHandler: SubscriptionHandler): boolean;
 
-   complete(): void
+  complete(): void;
 }
 
-export default class SubscriptionService<T> implements SubscriptionServiceInterface<T>{
-   history: {time: Date; data: T}[] = [];
-   private subscriptions: Map<number, (patchData: T) => void> = new Map<number,(patchData: T) => void>();
-   value: T;
+export default class SubscriptionService<T> implements SubscriptionServiceInterface<T> {
+  history: { time: Date; data: T }[] = [];
+  private subscriptions: Map<number, (patchData: T) => void> = new Map<number, (patchData: T) => void>();
+  value: T;
 
-   isComplete: boolean = false;
+  isComplete: boolean = false;
 
-   constructor(value: T) {
-      this.value = value;
-   }
+  constructor(value: T) {
+    this.value = value;
+  }
 
-   next(patchData: T): boolean {
-      if (this.isComplete) {
-         return false
-      }
-      this.history = [...this.history, {time:new Date(), data: this.value}]
-      this.value = patchData
-      this.subscriptions.forEach(callback => callback(this.value))
+  next(patchData: T): boolean {
+    if (this.isComplete) {
+      return false;
+    }
+    this.history = [...this.history, { time: new Date(), data: this.value }];
+    this.value = patchData;
+    this.subscriptions.forEach((callback) => callback(this.value));
 
-      return true;
-   }
+    return true;
+  }
 
-   subscription(patchDataCallback: (patchData: T) => void): SubscriptionHandler {
-      const subscriptionHandler = this.subscriptions.size + 1;
-      this.subscriptions.set(subscriptionHandler, patchDataCallback)
+  subscription(patchDataCallback: (patchData: T) => void): SubscriptionHandler {
+    const subscriptionHandler = this.subscriptions.size + 1;
+    this.subscriptions.set(subscriptionHandler, patchDataCallback);
 
-      return subscriptionHandler;
-   }
+    return subscriptionHandler;
+  }
 
-   unSubscription(subscriptionHandler: SubscriptionHandler): boolean {
-      return !this.subscriptions.has(subscriptionHandler) ?  false : this.subscriptions.delete(subscriptionHandler);
-   }
+  unSubscription(subscriptionHandler: SubscriptionHandler): boolean {
+    return !this.subscriptions.has(subscriptionHandler) ? false : this.subscriptions.delete(subscriptionHandler);
+  }
 
-   complete(): void {
-      this.isComplete = true;
-      this.subscriptions.clear()
-   }
+  complete(): void {
+    this.isComplete = true;
+    this.subscriptions.clear();
+  }
 }
 
 export const useObserve = <T>(observe: SubscriptionService<T>): [T, SubscriptionService<T>] => {
-   const [value, setValue] = useState<T>(observe.value)
-   useEffect(() => {
-      const subscriptionHandler = observe.subscription((newValue) =>
-         setValue(newValue)
-      )
+  const [value, setValue] = useState<T>(observe.value);
+  useEffect(() => {
+    const subscriptionHandler = observe.subscription((newValue) => setValue(newValue));
 
-      return () => {
-         observe.unSubscription(subscriptionHandler)
-      }
-   }, [])
+    return () => {
+      observe.unSubscription(subscriptionHandler);
+    };
+  }, []);
 
-   return [value, observe]
-}
+  return [value, observe];
+};
