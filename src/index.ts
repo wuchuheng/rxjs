e0cbdfc3609a6c2bf13fa4ac0d20401e025ce90f
wuchuheng <root@wuchuheng.com>
2022-01-19 23:31:44 +0800

    feat: tag version 0.0.1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,69 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import {useEffect, useState} from "react"
+
+type SubscriptionHandler = number
+
+interface SubscriptionServiceInterface<T> {
+   value: T;
+
+   next(patchData: T): boolean;
+
+   subscription(patchDataCallback:(patchData: T) => void ): SubscriptionHandler;
+
+   unSubscription(subscriptionHandler: SubscriptionHandler): boolean;
+
+   complete(): void
+}
+
+export default class SubscriptionService<T> implements SubscriptionServiceInterface<T>{
+   history: {time: Date; data: T}[] = [];
+   private subscriptions: Map<number, (patchData: T) => void> = new Map<number,(patchData: T) => void>();
+   value: T;
+
+   isComplete: boolean = false;
+
+   constructor(value: T) {
+      this.value = value;
+   }
+
+   next(patchData: T): boolean {
+      if (this.isComplete) {
+         return false
+      }
+      this.history = [...this.history, {time:new Date(), data: this.value}]
+      this.value = patchData
+      this.subscriptions.forEach(callback => callback(this.value))
+
+      return true;
+   }
+
+   subscription(patchDataCallback: (patchData: T) => void): SubscriptionHandler {
+      const subscriptionHandler = this.subscriptions.size + 1;
+      this.subscriptions.set(subscriptionHandler, patchDataCallback)
+
+      return subscriptionHandler;
+   }
+
+   unSubscription(subscriptionHandler: SubscriptionHandler): boolean {
+      return !this.subscriptions.has(subscriptionHandler) ?  false : this.subscriptions.delete(subscriptionHandler);
+   }
+
+   complete(): void {
+      this.isComplete = true;
+      this.subscriptions.clear()
+   }
+}
+
+export const useObserve = <T>(observe: SubscriptionService<T>): [T, SubscriptionService<T>] => {
+   const [value, setValue] = useState<T>(observe.value)
+   useEffect(() => {
+      const subscriptionHandler = observe.subscription((newValue) =>
+         setValue(newValue)
+      )
+
+      return () => {
+         observe.unSubscription(subscriptionHandler)
+      }
+   }, [])
+
+   return [value, observe]
+}
